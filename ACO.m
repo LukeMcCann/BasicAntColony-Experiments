3da4fa5d2180c61a008ef35dba4b3280beff22e4
LukeMcCann <developer-lukemccann@outlook.com>
2019-02-20 15:47:40 +0000

    Adds ACO foundation

---
+++ ACO.m
@@ -0,0 +1,34 @@
+close all 
+clc
+
+%% Create the Problem
+
+% Create Graph 
+[graph] = createGraph();
+
+% Draw
+figure
+subplot(1, 3, 1)
+drawGraph();
+
+
+%% ACO Algorithm
+
+% Initial Parameters
+max = 100;
+
+% Main Loop
+for t = 1 : max
+    % Create Colony
+    
+    % Calculate Fitness
+    
+    % Find Best Solution
+    
+    % Update Pheromone Matrix
+    
+    % Evaporate
+    
+    % Print Solution
+    
+end
