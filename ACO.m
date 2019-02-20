81fb61010dfb9ddfdff832d42a64cf8bc656582a
LukeMcCann <developer-lukemccann@outlook.com>
2019-02-20 16:51:02 +0000

    Adds values for tau and eta

--- ACO.m
+++ ACO.m
@@ -16,6 +16,17 @@ drawGraph(graph);
 
 % Initial Parameters
 max = 100;
+antPop = 10;
+
+% Calc the average of the distances between all edges in graph * #NO edges
+% Pheromone concentration
+tau0 = 10*1/( graph.n * mean(graph.edges(:) ));
+
+% Create pheramone matrices
+tau = tau0 * ones(graph.n, graph.n);
+
+% Edge desirability: shorter is more desirable
+eta = 1 ./graph.edges;
 
 % Main Loop
 for t = 1 : max
